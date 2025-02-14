f05693577d23735150aee087f791ad1119359397
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-02-14 19:57:49 +0200

    Add `UserDB` and it's suite

---
+++ include/UserDB.hpp
@@ -0,0 +1,13 @@
+#ifndef USERDB_HPP
+#define USERDB_HPP
+
+#include <iostream>
+
+
+class UserDB {
+public:
+    static int multiply(int a, int b);
+};
+
+
+#endif //USERDB_HPP
---
+++ src/UserDB.cpp
@@ -0,0 +1,5 @@
+#include "UserDB.hpp"
+
+int UserDB::multiply(int a, int b) {
+    return a * b;
+}
