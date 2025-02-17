7e8eb3dcc12ed2b872462c04b23937bc254b9428
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-02-17 22:57:38 +0200

    Add `input()` to `User`

--- include/User.hpp
+++ include/User.hpp
@@ -8,6 +8,8 @@ class User {
 private:
     static bool validate_int(const std::string &input);
 
+    static std::string input(const std::string &prompt, const std::string &data_type = "std::string");
+
 public:
     static int subtract(int a, int b);
 };
--- src/User.cpp
+++ src/User.cpp
@@ -7,3 +7,22 @@ int User::subtract(int a, int b) {
 bool User::validate_int(const std::string &input) {
     return all_of(input.begin(), input.end(), ::isdigit);
 }
+
+std::string User::input(const std::string &prompt, const std::string &data_type) {
+    std::string output;
+    bool askForInput = true;
+    while (askForInput) {
+        printf("%s", prompt.c_str());
+        std::getline(std::cin, output);
+        askForInput = false;
+        if (output.empty()) {
+            printf("Cannot accept empty input\n");
+            askForInput = true;
+        }
+        if (data_type == "int" && !validate_int(output)) {
+            printf("Cannot accept non-numeric input\n");
+            askForInput = true;
+        }
+    }
+    return output;
+}
