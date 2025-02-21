0d6284790d1bd7bf0d7204118ee781b539c641e5
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-02-21 03:20:49 +0200

    Add `bool` support in `User::input()`

--- include/User.hpp
+++ include/User.hpp
@@ -6,8 +6,11 @@
 #include <string>
 
 class User {
+private:
+    static bool _isValidInput(const std::string &input, const std::string &data_type = "std::string");
+
 public:
-    static std::string input(const std::string &prompt);
+    static std::string input(const std::string &prompt, const std::string &data_type = "std::string");
 
     static int subtract(const int &a, const int &b);
 };
--- src/User.cpp
+++ src/User.cpp
@@ -1,13 +1,24 @@
 #include "User.hpp"
 
-std::string User::input(const std::string &prompt) {
+bool User::_isValidInput(const std::string &input, const std::string &data_type) {
+    if (input.empty()) {
+        printf("Cannot accept empty input\n");
+        return false;
+    }
+    if ("bool" == data_type)
+        if (!("yes" == input || "no" == input)) {
+            printf("Can only accept 'yes' or 'no'\n");
+            return false;
+        }
+    return true;
+}
+
+std::string User::input(const std::string &prompt, const std::string &data_type) {
     std::string output;
-    while (output.empty()) {
+    do {
         printf("Enter %s:", prompt.c_str());
         std::getline(std::cin, output);
-        if (output.empty())
-            printf("Cannot accept empty input\n");
-    }
+    } while (!_isValidInput(output, data_type));
     return output;
 }
 
