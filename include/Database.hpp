26d2bbe357c3f90112757119a1f3c142af5f9f7c
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-02-24 20:45:18 +0200

    Add `Database.hpp` and its suite

---
+++ include/Database.hpp
@@ -0,0 +1,11 @@
+#ifndef DATABASE_HPP
+#define DATABASE_HPP
+
+
+class Database {
+public:
+    static int divide(const int &a, const int &b);
+};
+
+
+#endif //DATABASE_HPP
---
+++ src/Database.cpp
@@ -0,0 +1,6 @@
+#include "Database.hpp"
+
+int Database::divide(const int &a, const int &b) {
+    if (b == 0) return 0;
+    return a / b;
+}
