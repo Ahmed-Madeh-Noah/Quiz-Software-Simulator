90b336d6b80d5dfb8b5aaff8e59f46961224aee7
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2025-02-28 20:34:12 +0200

    Start using the `this` keyword

--- src/UserDB.cpp
+++ src/UserDB.cpp
@@ -6,23 +6,23 @@ int UserDB::get_users_count() const {
 }
 
 UserDB::UserDB() : rapidcsv::Document() {
-    if (!std::filesystem::exists(_fileName)) {
-        std::ofstream file(_fileName);
+    if (!std::filesystem::exists(this->_fileName)) {
+        std::ofstream file(this->_fileName);
         file.close();
     }
-    this->Load(_fileName, rapidcsv::LabelParams(0, 0));
+    this->Load(this->_fileName, rapidcsv::LabelParams(0, 0));
     if (this->GetColumnCount() == 0) {
         const std::vector<std::string> columnNames = {"Username", "Password", "Type"};
         const std::vector<std::string> placeholderData = {"Admin", "User"};
         for (size_t i = 0; i < columnNames.size(); ++i)
             this->InsertColumn(i - 1, placeholderData, columnNames[i]);
-        this->Save(_fileName);
-        this->Load(_fileName, rapidcsv::LabelParams(0, 0));
+        this->Save(this->_fileName);
+        this->Load(this->_fileName, rapidcsv::LabelParams(0, 0));
     }
 }
 
 UserDB::~UserDB() {
-    this->Save(_fileName);
+    this->Save(this->_fileName);
 }
 
 int UserDB::multiply(const int &a, const int &b) {
